861d94dfd292b4b84c49c9c7de02e4b1271a641a
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-17 23:20:56 -0600

    worked on fetch request

--- js/Bible-Lexicon-DB.js
+++ js/Bible-Lexicon-DB.js
@@ -1,18 +1,18 @@
-// const headers = {
-//     'Authorization': "Bearer " + DROPBOX_API_KEY,
-//     'Content-Type': "application/json",
-//     'X-Requested-With': "acr.browser.lightning",
-//     'credentials': "include"
-// };
-// const params = JSON.stringify({"title": "file request title", "destination": "/file request destination"});
-//
-// fetch('https://cors-anywhere.herokuapp.com/https://api.dropboxapi.com/2/file_requests/create', {
-//     method: 'GET',
-//     headers: headers,
-// })
-//     .then(res => res.json())
-//     .then(data => console.log('access data =>', data))
-//     .catch(err => console.error(err));
+const headers = {
+    'Authorization': "Bearer " + DROPBOX_API_KEY,
+    'Content-Type': "application/json",
+    'X-Requested-With': "acr.browser.lightning",
+    'credentials': "include"
+};
+const params = JSON.stringify({"title": "file request title", "destination": "/file request destination"});
+
+fetch('https://cors-anywhere.herokuapp.com/https://api.dropboxapi.com/2/file_requests/create', {
+    method: 'GET',
+    headers: headers,
+})
+    .then(res => res.json())
+    .then(data => console.log('access data =>', data))
+    .catch(err => console.error(err));
 
 
 // fetch('https://www.dropbox.com/oauth2/authorize?client_id=DROPBOX_API_KEY&redirect_uri=MY_REDIRECT_URI&response_type=code', {
