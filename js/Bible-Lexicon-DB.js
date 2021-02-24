7e6793e46533b5fe3e1f704278dc9004fb84c6e0
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-23 22:12:36 -0600

    building mysql DB....

--- js/Bible-Lexicon-DB.js
+++ js/Bible-Lexicon-DB.js
@@ -1,18 +1,30 @@
-const headers = {
-    'Authorization': "Bearer " + DROPBOX_API_KEY,
-    'Content-Type': "application/json",
-    'X-Requested-With': "acr.browser.lightning",
-    'credentials': "include"
-};
-const params = JSON.stringify({"title": "file request title", "destination": "/file request destination"});
+var mysql = require('mysql');
 
-fetch('https://cors-anywhere.herokuapp.com/https://api.dropboxapi.com/2/file_requests/create', {
-    method: 'GET',
-    headers: headers,
-})
-    .then(res => res.json())
-    .then(data => console.log('access data =>', data))
-    .catch(err => console.error(err));
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "joshuatalltree",
+    password: "Juliana2002!"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+// const headers = {
+//     'Authorization': "Bearer " + DROPBOX_API_KEY,
+//     'Content-Type': "application/json",
+//     'X-Requested-With': "acr.browser.lightning",
+//     'credentials': "include"
+// };
+// const params = JSON.stringify({"title": "file request title", "destination": "/file request destination"});
+//
+// fetch('https://cors-anywhere.herokuapp.com/https://api.dropboxapi.com/2/file_requests/create', {
+//     method: 'GET',
+//     headers: headers,
+// })
+//     .then(res => res.json())
+//     .then(data => console.log('access data =>', data))
+//     .catch(err => console.error(err));
 
 
 // fetch('https://www.dropbox.com/oauth2/authorize?client_id=DROPBOX_API_KEY&redirect_uri=MY_REDIRECT_URI&response_type=code', {
